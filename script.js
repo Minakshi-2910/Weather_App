ed195335e6c19e3f7eca46b5edd75c4cfa6a061a
Minakshi-2910 <137715212+Minakshi-2910@users.noreply.github.com>
2024-07-31 10:12:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle';
+const options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '925d78e568mshe6e2e28989eccafp19f7efjsn37eda54fda6b',
+		'x-rapidapi-host': 'weather-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
